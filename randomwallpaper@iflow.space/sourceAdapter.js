1e5dabe0b00062e64c8b4b136f7e764b60a917a2
Wolfgang Rumpler <contact@iflow.space>
2017-07-20 20:54:23 +0200

    Implement source settings.

--- randomwallpaper@iflow.space/sourceAdapter.js
+++ randomwallpaper@iflow.space/sourceAdapter.js
@@ -5,6 +5,11 @@ const Self = imports.misc.extensionUtils.getCurrentExtension();
 const Soup = imports.gi.Soup;
 const Json = imports.gi.Json;
 
+const RWG_SETTINGS_SCHEMA_DESKTOPPER = 'org.gnome.shell.extensions.space.iflow.randomwallpaper.desktopper';
+const RWG_SETTINGS_SCHEMA_UNSPLASH = 'org.gnome.shell.extensions.space.iflow.randomwallpaper.unsplash';
+const RWG_SETTINGS_SCHEMA_WALLHEAVEN = 'org.gnome.shell.extensions.space.iflow.randomwallpaper.wallheaven';
+
+const SettingsModule = Self.imports.settings;
 const HistoryModule = Self.imports.history;
 
 const LoggerModule = Self.imports.logger;
@@ -12,7 +17,6 @@ const LoggerModule = Self.imports.logger;
 let BaseAdapter = new Lang.Class({
 	Name: "BaseAdapter",
 	logger: null,
-
 
 	_init: function () {
 		this.logger = new LoggerModule.Logger('RWG3', 'BaseAdapter');
@@ -28,8 +32,7 @@ let BaseAdapter = new Lang.Class({
 		callback(null);
 	},
 
-	fileName: function(uri)
-	{
+	fileName: function (uri) {
 		let base = new String(uri).substring(uri.lastIndexOf('/') + 1);
 		return base;
 	},
@@ -40,18 +43,33 @@ let DesktopperAdapter = new Lang.Class({
 	Name: "DesktopperAdapter",
 	Extends: BaseAdapter,
 
+	_settings: null,
+
+	_init: function () {
+		this.parent();
+
+		this._settings = new SettingsModule.Settings(RWG_SETTINGS_SCHEMA_DESKTOPPER);
+	},
+
 	requestRandomImage: function (callback) {
 		let session = new Soup.SessionAsync();
-		let message = Soup.Message.new('GET', 'https://api.desktoppr.co/1/wallpapers/random');
 
-		let parser = new Json.Parser();
+		let url = 'https://api.desktoppr.co/1/wallpapers/random';
+		let allowUnsafe = this._settings.get('allow-unsafe', 'boolean');
+		if (allowUnsafe) {
+			url += '?safe_filter=all';
+		} else {
+			url += '?safe_filter=safe';
+		}
+		url = encodeURI(url);
+		this.logger.debug("Base URL: " + url);
+
+		let message = Soup.Message.new('GET', url);
 
 		session.queue_message(message, (session, message) => {
-			parser.load_from_data(message.response_body.data, -1);
-
-			let data = parser.get_root().get_object();
-			let response = data.get_object_member('response');
-			let imageUrl = response.get_object_member('image').get_string_member('url');
+			let data = JSON.parse(message.response_body.data);
+			let response = data.response;
+			let imageUrl = encodeURI(response.image.url);
 
 			if (callback) {
 				let historyEntry = new HistoryModule.HistoryEntry(null, 'desktopper.co', imageUrl);
@@ -69,10 +87,32 @@ let UnsplashAdapter = new Lang.Class({
 	sourceName: 'Unsplash',
 	sourceUrl: 'https://unsplash.com/',
 
+	_settings: null,
+
+	// query options
+	options: {
+		'username': '',
+		'query': '',
+		'w': 1920,
+		'h': 1080,
+	},
+
+	_init: function () {
+		this.parent();
+
+		this._settings = new SettingsModule.Settings(RWG_SETTINGS_SCHEMA_UNSPLASH);
+	},
+
 	requestRandomImage: function (callback) {
 		let session = new Soup.SessionAsync();
-		let url = 'https://api.unsplash.com/photos/random';
-		url += '?client_id=64daf439e9b579dd566620c0b07022706522d87b255d06dd01d5470b7f193b8d';
+
+		this._readOptionsFromSettings();
+		let optionsString = this._generateOptionsString();
+		let url = 'https://api.unsplash.com/photos/random?' + optionsString;
+		url += 'client_id=64daf439e9b579dd566620c0b07022706522d87b255d06dd01d5470b7f193b8d';
+		url = encodeURI(url);
+		this.logger.debug("Base URL: " + url);
+
 		let message = Soup.Message.new('GET', url);
 
 		let utmParameters = '?utm_source=RandomWallpaperGnome3&utm_medium=referral&utm_campaign=api-credit';
@@ -80,59 +120,75 @@ let UnsplashAdapter = new Lang.Class({
 		session.queue_message(message, (session, message) => {
 			let data = JSON.parse(message.response_body.data);
 
-			let imageUrl = data.links.download;
+			let imageUrl = encodeURI(data.links.download + utmParameters);
 			let authorName = data.user.name;
-			let authorUrl = data.user.links.html;
-
-			this.logger.debug(authorName);
-			this.logger.debug(authorUrl);
+			let authorUrl = encodeURI(data.user.links.html);
 
 			if (callback) {
-				let historyEntry = new HistoryModule.HistoryEntry(authorName, this.sourceName, imageUrl+utmParameters);
-				historyEntry.source.sourceUrl = this.sourceUrl+utmParameters;
-				historyEntry.source.authorUrl = authorUrl+utmParameters;
+				let historyEntry = new HistoryModule.HistoryEntry(authorName, this.sourceName, encodeURI(imageUrl));
+				historyEntry.source.sourceUrl = encodeURI(this.sourceUrl + utmParameters);
+				historyEntry.source.authorUrl = encodeURI(authorUrl + utmParameters);
 				callback(historyEntry);
 			}
 		});
+	},
+
+	_generateOptionsString: function () {
+		let options = this.options;
+		let optionsString = "";
+
+		for (let key in options) {
+			if (options.hasOwnProperty(key)) {
+				if (options[key]) {
+					optionsString += key + "=" + options[key] + "&";
+				}
+			}
+		}
+
+		return optionsString;
+	},
+
+	_readOptionsFromSettings: function () {
+		this.options.query = this._settings.get('unsplash-keyword', 'string');
+
+		this.options.username = this._settings.get('username', 'string');
+		if (this.options.username[0] === '@') {
+			this.options.username = this.options.username.substring(1); // remove @ prefix
+		}
+
+		this.options.w = this._settings.get('image-width', 'int');
+		this.options.h = this._settings.get('image-height', 'int');
 	}
 });
 
 let WallheavenAdapter = new Lang.Class({
 	Name: "WallheavenAdapter",
 	Extends: BaseAdapter,
+	_settings: null,
 
 	// query options
 	options: {
 		'q': '',
 		'purity': '110', // SFW, sketchy
 		'sorting': 'random',
-		'category': '111', // General, Anime, People
+		'categories': '111', // General, Anime, People
 		'resolutions': ['1920x1200', '2560x1440']
 	},
 
-	/*
-	 fetch a random image url from wallheaven.cc with the given options
-	 and call callback function with the URL of the image
-	 */
+	_init: function () {
+		this.parent();
+
+		this._settings = new SettingsModule.Settings(RWG_SETTINGS_SCHEMA_WALLHEAVEN);
+	},
+
 	requestRandomImage: function (callback) {
 		let session = new Soup.SessionAsync();
 
-		let options = this.options;
-		let optionsString = "";
-
-		for (let key in options) {
-			if (options.hasOwnProperty(key)) {
-				if (Array.isArray(options[key])) {
-					optionsString += key + "=" + options[key].join() + "&";
-				} else {
-					optionsString += key + "=" + options[key] + "&";
-				}
-			}
-		}
-		// remove last '&'
-		optionsString = optionsString.slice(0, -1);
-
+		this._readOptionsFromSettings();
+		let optionsString = this._generateOptionsString();
 		let url = 'http://alpha.wallhaven.cc/search?' + optionsString;
+		url = encodeURI(url);
+		this.logger.debug("Base URL: " + url);
 
 		let message = Soup.Message.new('GET', url);
 
@@ -155,6 +211,7 @@ let WallheavenAdapter = new Lang.Class({
 				let imageUrl = body.match(new RegExp(/\/\/wallpapers.wallhaven.cc\/wallpapers\/full\/.*?"/))[0];
 				imageUrl = imageUrl.slice(0, -1);
 				imageUrl = 'http:' + imageUrl;
+				imageUrl = encodeURI(imageUrl);
 
 				if (callback) {
 					let historyEntry = new HistoryModule.HistoryEntry(null, 'wallhaven.cc', imageUrl);
@@ -165,5 +222,45 @@ let WallheavenAdapter = new Lang.Class({
 
 
 		});
+	},
+
+	_generateOptionsString: function () {
+		let options = this.options;
+		let optionsString = "";
+
+		for (let key in options) {
+			if (options.hasOwnProperty(key)) {
+				if (Array.isArray(options[key])) {
+					optionsString += key + "=" + options[key].join() + "&";
+				} else {
+					if (options[key]) {
+						optionsString += key + "=" + options[key] + "&";
+					}
+				}
+			}
+		}
+
+		return optionsString;
+	},
+
+	_readOptionsFromSettings: function () {
+		this.options.q = this._settings.get('wallheaven-keyword', 'string');
+
+		this.options.resolutions = this._settings.get('resolutions', 'string').split(',');
+		this.options.resolutions = this.options.resolutions.map((elem) => {
+			return elem.trim();
+		});
+
+		let categories = [];
+		categories.push(+this._settings.get('category-general', 'boolean')); // + is implicit conversion to int
+		categories.push(+this._settings.get('category-anime', 'boolean'));
+		categories.push(+this._settings.get('category-people', 'boolean'));
+		this.options.categories = categories.join('');
+
+		let purity = [];
+		purity.push(+this._settings.get('allow-sfw', 'boolean'));
+		purity.push(+this._settings.get('allow-sketchy', 'boolean'));
+		purity.push(0); // required by wallheaven
+		this.options.purity = purity.join('');
 	}
 });
