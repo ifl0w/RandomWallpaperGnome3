9dada38c9708a665d873a32bd61cfcfde0c005b9
Lucki <Lucki@holarse.de>
2022-12-11 23:57:59 +0100

    Rework UI in preparation for multiple sources

--- randomwallpaper@iflow.space/sourceAdapter.js
+++ randomwallpaper@iflow.space/sourceAdapter.js
@@ -58,7 +58,7 @@ var BaseAdapter = class {
 	}
 
 	_error(err, callback) {
-		let error = {"error": err};
+		let error = { "error": err };
 		this.logger.error(JSON.stringify(error));
 
 		if (callback) {
@@ -220,8 +220,8 @@ var WallhavenAdapter = class extends BaseAdapter {
 			let siteURL = entry.url;
 
 			let apiKey = this.options["apikey"];
-			if(apiKey){
-				downloadURL += "?apikey="+apiKey;
+			if (apiKey) {
+				downloadURL += "?apikey=" + apiKey;
 			}
 
 			if (callback) {
@@ -359,34 +359,54 @@ var GenericJsonAdapter = class extends BaseAdapter {
 				const response_body = JSON.parse(ByteArray.toString(response_body_bytes));
 
 				let identifier = this._settings.get("generic-json-id", "string");
-				let imageJSONPath = this._settings.get("generic-json-response-path", "string");
-				let linkJSONPath = this._settings.get("generic-json-link-path", "string");
+				let imageJSONPath = this._settings.get("generic-json-image-path", "string");
+				let postJSONPath = this._settings.get("generic-json-post-path", "string");
 				let domainUrl = this._settings.get("generic-json-domain", "string");
+				let authorNameJSONPath = this._settings.get("generic-json-author-name-path", "string");
+				let authorUrlJSONPath = this._settings.get("generic-json-author-url-path", "string");
 
 				if (identifier === null || identifier === "") {
 					identifier = 'Generic JSON Source';
 				}
 
 				let rObject = this._jsonPathParser.access(response_body, imageJSONPath);
-				let imageDownloadUrl = this._settings.get("generic-json-url-prefix", "string") + rObject.Object;
-				
+				let imageDownloadUrl = this._settings.get("generic-json-image-prefix", "string") + rObject.Object;
+
 				// '@random' would yield different results so lets make sure the values stay
 				// the same as long as the path is identical
-				let samePath = imageJSONPath.substring(0, this.findFirstDifference(imageJSONPath, linkJSONPath));
+				let samePath = imageJSONPath.substring(0, this.findFirstDifference(imageJSONPath, postJSONPath));
 
 				// count occurrences of '@random' to slice the array later
 				// https://stackoverflow.com/a/4009768
 				let occurrences = (samePath.match(/@random/g) || []).length;
+				let slicedRandomElements = rObject.RandomElements.slice(0, occurrences);
 
-				let linkUrl = this._jsonPathParser.access(response_body, linkJSONPath, rObject.RandomElements.slice(0, occurrences), false).Object;
-				linkUrl = this._settings.get("generic-json-link-prefix", "string") + linkUrl;
+				let postUrl = this._jsonPathParser.access(response_body, postJSONPath, slicedRandomElements, false).Object;
+				postUrl = this._settings.get("generic-json-post-prefix", "string") + postUrl;
+				if (typeof postUrl !== 'string' || !postUrl instanceof String) {
+					postUrl = null;
+				}
+
+				let authorName = this._jsonPathParser.access(response_body, authorNameJSONPath, slicedRandomElements, false).Object;
+				if (typeof authorName !== 'string' || !authorName instanceof String) {
+					authorName = null;
+				}
+
+				let authorUrl = this._jsonPathParser.access(response_body, authorUrlJSONPath, slicedRandomElements, false).Object;
+				authorUrl = this._settings.get("generic-json-author-url-prefix", "string") + authorUrl;
+				if (typeof authorUrl !== 'string' || !authorUrl instanceof String) {
+					authorUrl = null;
+				}
 
 				if (callback) {
-					let historyEntry = new HistoryModule.HistoryEntry(null, identifier, rObject.Object);
-					historyEntry.source.sourceUrl = imageDownloadUrl;
+					let historyEntry = new HistoryModule.HistoryEntry(authorName, identifier, imageDownloadUrl);
 
-					if (linkUrl !== null && linkUrl !== "") {
-						historyEntry.source.imageLinkUrl = linkUrl;
+					if (authorUrl !== null && authorUrl !== "") {
+						historyEntry.source.authorUrl = authorUrl;
+					}
+
+					if (postUrl !== null && postUrl !== "") {
+						historyEntry.source.imageLinkUrl = postUrl;
 					}
 
 					if (domainUrl !== null && domainUrl !== "") {
@@ -403,7 +423,7 @@ var GenericJsonAdapter = class extends BaseAdapter {
 	}
 
 	// https://stackoverflow.com/a/32859917
-	findFirstDifference (jsonPath1, jsonPath2) {
+	findFirstDifference(jsonPath1, jsonPath2) {
 		let i = 0;
 		if (jsonPath1 === jsonPath2) return -1;
 		while (jsonPath1[i] === jsonPath2[i]) i++;
