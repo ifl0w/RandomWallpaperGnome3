1cb51ef9d389c024489ca14ad2fde2eb881d5991
Wolfgang Rumpler <contact@iflow.space>
2022-12-11 18:15:31 +0100

    only log warnings and errors by default

--- randomwallpaper@iflow.space/logger.js
+++ randomwallpaper@iflow.space/logger.js
@@ -1,3 +1,15 @@
+// TODO: use an enum once moved to TS
+const LOG_LEVEL = {
+	SILENT: 0,
+	ERROR: 1,
+	WARN: 2,
+	INFO: 3,
+	DEBUG: 4,
+}
+
+// TODO: add UI option or at least ENV variable (this is a quick workaround to conform to extension review requirements)
+const CURRENT_LOG_LEVEL = LOG_LEVEL.WARN;
+
 var Logger = class {
 
 	constructor(prefix, callingClass) {
@@ -10,18 +22,30 @@ var Logger = class {
 	}
 
 	debug(message) {
+		if (CURRENT_LOG_LEVEL < LOG_LEVEL.DEBUG)
+			return;
+
 		this._log('DEBUG', message);
 	}
 
 	info(message) {
+		if (CURRENT_LOG_LEVEL < LOG_LEVEL.INFO)
+			return;
+
 		this._log('INFO', message);
 	}
 
 	warn(message) {
+		if (CURRENT_LOG_LEVEL < LOG_LEVEL.WARN)
+			return;
+
 		this._log('WARNING', message);
 	}
 
 	error(message) {
+		if (CURRENT_LOG_LEVEL < LOG_LEVEL.ERROR)
+			return;
+
 		this._log('ERROR', message);
 	}
 
