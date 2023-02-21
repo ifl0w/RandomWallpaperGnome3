6e7a75e8d2a41e4518eab6fb114ce806d7f73c51
Lucki <Lucki@holarse.de>
2023-02-21 15:54:29 +0100

    Random whitespace changes

--- randomwallpaper@iflow.space/ui/unsplash.js
+++ randomwallpaper@iflow.space/ui/unsplash.js
@@ -10,67 +10,67 @@ const Convenience = Self.imports.convenience;
 const RWG_SETTINGS_SCHEMA_UNSPLASH = 'org.gnome.shell.extensions.space.iflow.randomwallpaper.sources.unsplash';
 
 var UnsplashSettingsGroup = GObject.registerClass({
-    GTypeName: 'UnsplashSettingsGroup',
-    Template: GLib.filename_to_uri(Self.path + '/ui/unsplash.ui', null),
-    InternalChildren: [
-        'constraint_type',
-        'constraint_value',
-        'featured_only',
-        'image_height',
-        'image_width',
-        'keyword'
-    ]
+	GTypeName: 'UnsplashSettingsGroup',
+	Template: GLib.filename_to_uri(Self.path + '/ui/unsplash.ui', null),
+	InternalChildren: [
+		'constraint_type',
+		'constraint_value',
+		'featured_only',
+		'image_height',
+		'image_width',
+		'keyword'
+	]
 }, class UnsplashSettingsGroup extends Adw.PreferencesGroup {
-    constructor(parent_row, params = {}) {
-        super(params);
+	constructor(parent_row, params = {}) {
+		super(params);
 
-        const path = `/org/gnome/shell/extensions/space-iflow-randomwallpaper/sources/unsplash/${parent_row.id}/`;
-        this._settings = Convenience.getSettings(RWG_SETTINGS_SCHEMA_UNSPLASH, path);
+		const path = `/org/gnome/shell/extensions/space-iflow-randomwallpaper/sources/unsplash/${parent_row.id}/`;
+		this._settings = Convenience.getSettings(RWG_SETTINGS_SCHEMA_UNSPLASH, path);
 
-        this._settings.bind('name',
-            parent_row.source_name,
-            'text',
-            Gio.SettingsBindFlags.DEFAULT);
-        this._settings.bind('keyword',
-            this._keyword,
-            'text',
-            Gio.SettingsBindFlags.DEFAULT);
-        this._settings.bind('image-width',
-            this._image_width,
-            'value',
-            Gio.SettingsBindFlags.DEFAULT);
-        this._settings.bind('image-height',
-            this._image_height,
-            'value',
-            Gio.SettingsBindFlags.DEFAULT);
-        this._settings.bind('featured-only',
-            this._featured_only,
-            'active',
-            Gio.SettingsBindFlags.DEFAULT);
-        this._settings.bind('constraint-type',
-            this._constraint_type,
-            'selected',
-            Gio.SettingsBindFlags.DEFAULT);
-        this._settings.bind('constraint-value',
-            this._constraint_value,
-            'text',
-            Gio.SettingsBindFlags.DEFAULT);
+		this._settings.bind('name',
+			parent_row.source_name,
+			'text',
+			Gio.SettingsBindFlags.DEFAULT);
+		this._settings.bind('keyword',
+			this._keyword,
+			'text',
+			Gio.SettingsBindFlags.DEFAULT);
+		this._settings.bind('image-width',
+			this._image_width,
+			'value',
+			Gio.SettingsBindFlags.DEFAULT);
+		this._settings.bind('image-height',
+			this._image_height,
+			'value',
+			Gio.SettingsBindFlags.DEFAULT);
+		this._settings.bind('featured-only',
+			this._featured_only,
+			'active',
+			Gio.SettingsBindFlags.DEFAULT);
+		this._settings.bind('constraint-type',
+			this._constraint_type,
+			'selected',
+			Gio.SettingsBindFlags.DEFAULT);
+		this._settings.bind('constraint-value',
+			this._constraint_value,
+			'text',
+			Gio.SettingsBindFlags.DEFAULT);
 
-        this._unsplashUnconstrained(this._constraint_type, true, this._featured_only);
-        this._unsplashUnconstrained(this._constraint_type, false, this._constraint_value);
-        this._constraint_type.connect('notify::selected', (comboRow) => {
-            this._unsplashUnconstrained(comboRow, true, this._featured_only);
-            this._unsplashUnconstrained(comboRow, false, this._constraint_value);
+		this._unsplashUnconstrained(this._constraint_type, true, this._featured_only);
+		this._unsplashUnconstrained(this._constraint_type, false, this._constraint_value);
+		this._constraint_type.connect('notify::selected', (comboRow) => {
+			this._unsplashUnconstrained(comboRow, true, this._featured_only);
+			this._unsplashUnconstrained(comboRow, false, this._constraint_value);
 
-            this._featured_only.set_active(false);
-        });
-    }
+			this._featured_only.set_active(false);
+		});
+	}
 
-    _unsplashUnconstrained(comboRow, enable, targetElement) {
-        if (comboRow.selected === 0) {
-            targetElement.set_sensitive(enable);
-        } else {
-            targetElement.set_sensitive(!enable);
-        }
-    }
+	_unsplashUnconstrained(comboRow, enable, targetElement) {
+		if (comboRow.selected === 0) {
+			targetElement.set_sensitive(enable);
+		} else {
+			targetElement.set_sensitive(!enable);
+		}
+	}
 });
