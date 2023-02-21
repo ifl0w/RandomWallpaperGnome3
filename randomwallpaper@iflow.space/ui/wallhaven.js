0ab3213214dc1ff59351bcc4c8a2b28d930c81e2
Lucki <Lucki@holarse.de>
2023-02-21 15:54:29 +0100

    Random whitespace changes

--- randomwallpaper@iflow.space/ui/wallhaven.js
+++ randomwallpaper@iflow.space/ui/wallhaven.js
@@ -10,65 +10,65 @@ const Convenience = Self.imports.convenience;
 const RWG_SETTINGS_SCHEMA_WALLHAVEN = 'org.gnome.shell.extensions.space.iflow.randomwallpaper.sources.wallhaven';
 
 var WallhavenSettingsGroup = GObject.registerClass({
-    GTypeName: 'WallhavenSettingsGroup',
-    Template: GLib.filename_to_uri(Self.path + '/ui/wallhaven.ui', null),
-    InternalChildren: [
-        'allow_sfw',
-        'allow_sketchy',
-        'allow_nsfw',
-        'api_key',
-        'category_anime',
-        'category_general',
-        'category_people',
-        'keyword',
-        'resolutions'
-    ]
+	GTypeName: 'WallhavenSettingsGroup',
+	Template: GLib.filename_to_uri(Self.path + '/ui/wallhaven.ui', null),
+	InternalChildren: [
+		'allow_sfw',
+		'allow_sketchy',
+		'allow_nsfw',
+		'api_key',
+		'category_anime',
+		'category_general',
+		'category_people',
+		'keyword',
+		'resolutions'
+	]
 }, class WallhavenSettingsGroup extends Adw.PreferencesGroup {
-    constructor(parent_row, params = {}) {
-        super(params);
+	constructor(parent_row, params = {}) {
+		super(params);
 
-        const path = `/org/gnome/shell/extensions/space-iflow-randomwallpaper/sources/wallhaven/${parent_row.id}/`;
-        this._settings = Convenience.getSettings(RWG_SETTINGS_SCHEMA_WALLHAVEN, path);
+		const path = `/org/gnome/shell/extensions/space-iflow-randomwallpaper/sources/wallhaven/${parent_row.id}/`;
+		this._settings = Convenience.getSettings(RWG_SETTINGS_SCHEMA_WALLHAVEN, path);
 
-        this._settings.bind('name',
-            parent_row.source_name,
-            'text',
-            Gio.SettingsBindFlags.DEFAULT);
-        this._settings.bind('keyword',
-            this._keyword,
-            'text',
-            Gio.SettingsBindFlags.DEFAULT);
-        this._settings.bind('api-key',
-            this._api_key,
-            'text',
-            Gio.SettingsBindFlags.DEFAULT);
-        this._settings.bind('resolutions',
-            this._resolutions,
-            'text',
-            Gio.SettingsBindFlags.DEFAULT);
-        this._settings.bind('category-general',
-            this._category_general,
-            'active',
-            Gio.SettingsBindFlags.DEFAULT);
-        this._settings.bind('category-anime',
-            this._category_anime,
-            'active',
-            Gio.SettingsBindFlags.DEFAULT);
-        this._settings.bind('category-people',
-            this._category_people,
-            'active',
-            Gio.SettingsBindFlags.DEFAULT);
-        this._settings.bind('allow-sfw',
-            this._allow_sfw,
-            'active',
-            Gio.SettingsBindFlags.DEFAULT);
-        this._settings.bind('allow-sketchy',
-            this._allow_sketchy,
-            'active',
-            Gio.SettingsBindFlags.DEFAULT);
-        this._settings.bind('allow-nsfw',
-            this._allow_nsfw,
-            'active',
-            Gio.SettingsBindFlags.DEFAULT);
-    }
+		this._settings.bind('name',
+			parent_row.source_name,
+			'text',
+			Gio.SettingsBindFlags.DEFAULT);
+		this._settings.bind('keyword',
+			this._keyword,
+			'text',
+			Gio.SettingsBindFlags.DEFAULT);
+		this._settings.bind('api-key',
+			this._api_key,
+			'text',
+			Gio.SettingsBindFlags.DEFAULT);
+		this._settings.bind('resolutions',
+			this._resolutions,
+			'text',
+			Gio.SettingsBindFlags.DEFAULT);
+		this._settings.bind('category-general',
+			this._category_general,
+			'active',
+			Gio.SettingsBindFlags.DEFAULT);
+		this._settings.bind('category-anime',
+			this._category_anime,
+			'active',
+			Gio.SettingsBindFlags.DEFAULT);
+		this._settings.bind('category-people',
+			this._category_people,
+			'active',
+			Gio.SettingsBindFlags.DEFAULT);
+		this._settings.bind('allow-sfw',
+			this._allow_sfw,
+			'active',
+			Gio.SettingsBindFlags.DEFAULT);
+		this._settings.bind('allow-sketchy',
+			this._allow_sketchy,
+			'active',
+			Gio.SettingsBindFlags.DEFAULT);
+		this._settings.bind('allow-nsfw',
+			this._allow_nsfw,
+			'active',
+			Gio.SettingsBindFlags.DEFAULT);
+	}
 });
