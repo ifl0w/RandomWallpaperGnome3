0d0ff14e8711568072617de36d8371c14f36dfe4
Lucki <Lucki@holarse.de>
2023-05-29 14:13:22 +0200

    Refactor URL source adapter and settings
    Provide a switch to declare a URL is responding with different images
    when queried in a short amount of time.

--- src/ui/urlSource.ts
+++ src/ui/urlSource.ts
@@ -2,6 +2,7 @@ import * as Adw from '@gi-types/adw1';
 import * as Gio from 'gi://Gio';
 import * as GLib from 'gi://GLib';
 import * as GObject from 'gi://GObject';
+import * as Gtk from 'gi://Gtk?version=4.0';
 
 import * as AdwEntryRow from '@gi/gtk4/adw/adwEntryRow';
 import * as ExtensionUtils from '@gi/misc/extensionUtils';
@@ -16,6 +17,7 @@ const UrlSourceSettingsGroup = GObject.registerClass({
     InternalChildren: [
         'author_name',
         'author_url',
+        'different_images',
         'domain',
         'image_url',
         'post_url',
@@ -24,6 +26,7 @@ const UrlSourceSettingsGroup = GObject.registerClass({
     // InternalChildren
     private _author_name!: AdwEntryRow.EntryRow;
     private _author_url!: AdwEntryRow.EntryRow;
+    private _different_images!: Gtk.Switch;
     private _domain!: AdwEntryRow.EntryRow;
     private _image_url!: AdwEntryRow.EntryRow;
     private _post_url!: AdwEntryRow.EntryRow;
@@ -43,6 +46,10 @@ const UrlSourceSettingsGroup = GObject.registerClass({
         this._settings.bind('author-url',
             this._author_url,
             'text',
+            Gio.SettingsBindFlags.DEFAULT);
+        this._settings.bind('different-images',
+            this._different_images,
+            'active',
             Gio.SettingsBindFlags.DEFAULT);
         this._settings.bind('domain',
             this._domain,
