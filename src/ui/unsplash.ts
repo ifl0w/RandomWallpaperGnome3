0b5f23791187267d27cebbb3d49ab0f870e03feb
Lucki <Lucki@holarse.de>
2023-07-08 17:39:48 +0200

    Remove constraint-type enum from gsettings

--- src/ui/unsplash.ts
+++ src/ui/unsplash.ts
@@ -8,6 +8,7 @@ import Gtk from 'gi://Gtk';
 const ExtensionUtils = imports.misc.extensionUtils;
 
 import * as Settings from './../settings.js';
+import {getConstraintTypeNameList} from '../adapter/unsplash.js';
 
 const Self = ExtensionUtils.getCurrentExtension();
 
@@ -50,25 +51,22 @@ const UnsplashSettingsGroup = GObject.registerClass({
         const path = `${Settings.RWG_SETTINGS_SCHEMA_PATH}/sources/unsplash/${id}/`;
         this._settings = new Settings.Settings(Settings.RWG_SETTINGS_SCHEMA_SOURCES_UNSPLASH, path);
 
-        if (!UnsplashSettingsGroup._stringList) {
-            // Fill combo from settings enum
-
-            const availableTypes = this._settings.getSchema().get_key('constraint-type').get_range(); // GLib.Variant (sv)
-            // (sv) = Tuple(%G_VARIANT_TYPE_STRING, %G_VARIANT_TYPE_VARIANT)
-            // s should be 'enum'
-            // v should be an array enumerating the possible values. Each item in the array is a possible valid value and no other values are valid.
-            // v is 'as'
-            const availableTypeNames = availableTypes.get_child_value(1).get_variant().get_strv();
-
-            UnsplashSettingsGroup._stringList = Gtk.StringList.new(availableTypeNames);
-        }
+        if (!UnsplashSettingsGroup._stringList)
+            UnsplashSettingsGroup._stringList = Gtk.StringList.new(getConstraintTypeNameList());
 
         this._constraint_type.model = UnsplashSettingsGroup._stringList;
-        this._constraint_type.selected = this._settings.getEnum('constraint-type');
 
-        this._settings.bind('keyword',
-            this._keyword,
+        this._settings.bind('constraint-type',
+            this._constraint_type,
+            'selected',
+            Gio.SettingsBindFlags.DEFAULT);
+        this._settings.bind('constraint-value',
+            this._constraint_value,
             'text',
+            Gio.SettingsBindFlags.DEFAULT);
+        this._settings.bind('featured-only',
+            this._featured_only,
+            'active',
             Gio.SettingsBindFlags.DEFAULT);
         this._settings.bind('image-width',
             this._image_width,
@@ -78,15 +76,8 @@ const UnsplashSettingsGroup = GObject.registerClass({
             this._image_height,
             'value',
             Gio.SettingsBindFlags.DEFAULT);
-        this._settings.bind('featured-only',
-            this._featured_only,
-            'active',
-            Gio.SettingsBindFlags.DEFAULT);
-
-        // Binding an enum isn't possible straight away.
-        // This would need bind_with_mapping() which isn't available in gjs?
-        this._settings.bind('constraint-value',
-            this._constraint_value,
+        this._settings.bind('keyword',
+            this._keyword,
             'text',
             Gio.SettingsBindFlags.DEFAULT);
 
@@ -95,7 +86,6 @@ const UnsplashSettingsGroup = GObject.registerClass({
         this._constraint_type.connect('notify::selected', (comboRow: Adw.ComboRow) => {
             this._unsplashUnconstrained(comboRow, true, this._featured_only);
             this._unsplashUnconstrained(comboRow, false, this._constraint_value);
-            this._settings.setEnum('constraint-type', comboRow.selected);
 
             this._featured_only.set_active(false);
         });
