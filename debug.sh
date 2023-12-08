e9d4dc65fb31a2c381c914ed3d36ac2fd158e9f6
Wolfgang Rumpler <contact@iflow.space>
2023-12-08 20:46:44 +0100

    Adapt debug.sh for gnome-shell 45
    Filtering the journal via the extension UUID was removed in version 45
    [1]. Adapt the debug.sh to follow the entire gnome-shell logs with the
    option to filter using "grep".
    
    [1] https://gjs.guide/extensions/upgrading/gnome-shell-45.html#logging

--- debug.sh
+++ debug.sh
@@ -2,7 +2,12 @@
 
 if [ "$1" = "prefs" ]; then
   journalctl -f /usr/bin/gjs
+elif [ "$1" = "filtered" ]; then
+  # Note: filtering journal via the extensions UUID was removed in 45.
+  # Falling back to grep log filtering. This is only necessary when other
+  # extensions produce to much output in the gnome-shell log.
+  # https://gjs.guide/extensions/upgrading/gnome-shell-45.html#logging
+  journalctl -f /usr/bin/gnome-shell | grep "RandomWallpaper"
 else
-  # https://gjs.guide/extensions/development/debugging.html#logging
-  journalctl -f GNOME_SHELL_EXTENSION_UUID=randomwallpaper@iflow.space
+  journalctl -f /usr/bin/gnome-shell
 fi
