48ed15a21656a4c0dbb4c4bbd38bac79ad3b0895
Wolfgang Rumpler <contact@iflow.space>
2024-09-20 00:08:51 +0200

    Re-implement unsplash using the official API
    Due to deprecation of "source.unsplash.com" Unsplash requests are
    failing. The remaining solution is to use the official API that requires
    an API key.
    
    * BREAKING CHANGE: Add new unsplash schema to not support the old one
      anymore

--- src/ui/unsplash.ts
+++ src/ui/unsplash.ts
@@ -6,19 +6,34 @@ import Gtk from 'gi://Gtk';
 
 import * as Settings from './../settings.js';
 
+/**
+ * Local shim of translate function until supported with import below:
+ * import {gettext as _} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
+ *
+ * @param {string} text to translate
+ * @returns {string} same text
+ */
+function _(text: string): string {
+    return text;
+}
+
 // Generated code produces a no-shadow rule error
 /* eslint-disable */
-enum ConstraintType {
-    UNCONSTRAINED,
-    USER,
-    USERS_LIKES,
-    COLLECTION_ID,
+// the strings represent the API supported values
+enum ContentFilter {
+    HIGH = 'high',
+    LOW = 'low',
+}
+// the strings represent the API supported values
+enum Orientation {
+    LANDSCAPE = 'landscape',
+    PORTRAIT = 'portrait',
+    SQUARISH = 'squarish',
 }
 /* eslint-enable */
 
 // FIXME: Generated static class code produces a no-unused-expressions rule error
 /* eslint-disable no-unused-expressions */
-
 /**
  * Subclass containing the preferences for Unsplash adapter
  */
@@ -28,21 +43,23 @@ class UnsplashSettings extends Adw.PreferencesPage {
     static [Gtk.template] = GLib.uri_resolve_relative(import.meta.url, './unsplash.ui', GLib.UriFlags.NONE);
     // @ts-expect-error Gtk.internalChildren is not in the type definitions files yet
     static [Gtk.internalChildren] = [
-        'constraint_type',
-        'constraint_value',
-        'featured_only',
-        'image_height',
-        'image_width',
-        'keyword',
+        'api_key',
+        'query',
+        'username',
+        'topics',
+        'collections',
+        'orientation',
+        'content_filter',
     ];
 
     // InternalChildren
-    private _constraint_type!: Adw.ComboRow;
-    private _constraint_value!: Adw.EntryRow;
-    private _featured_only!: Gtk.Switch;
-    private _image_height!: Gtk.Adjustment;
-    private _image_width!: Gtk.Adjustment;
-    private _keyword!: Adw.EntryRow;
+    private _collections!: Adw.EntryRow;
+    private _topics!: Adw.EntryRow;
+    private _username!: Adw.EntryRow;
+    private _query!: Adw.EntryRow;
+    private _api_key!: Adw.EntryRow;
+    private _orientation!: Adw.ComboRow;
+    private _content_filter!: Adw.ComboRow;
 
     static {
         GObject.registerClass(this);
@@ -66,59 +83,98 @@ class UnsplashSettings extends Adw.PreferencesPage {
         const path = `${Settings.RWG_SETTINGS_SCHEMA_PATH}/sources/unsplash/${id}/`;
         this._settings = new Settings.Settings(Settings.RWG_SETTINGS_SCHEMA_SOURCES_UNSPLASH, path);
 
-        if (!UnsplashSettings._stringList)
-            UnsplashSettings._stringList = Gtk.StringList.new(getConstraintTypeNameList());
-
-        this._constraint_type.model = UnsplashSettings._stringList;
-
-        this._settings.bind('constraint-type',
-            this._constraint_type,
-            'selected',
-            Gio.SettingsBindFlags.DEFAULT);
-        this._settings.bind('constraint-value',
-            this._constraint_value,
+        this._settings.bind('api-key',
+            this._api_key,
             'text',
             Gio.SettingsBindFlags.DEFAULT);
-        this._settings.bind('featured-only',
-            this._featured_only,
-            'active',
+        this._settings.bind('username',
+            this._username,
+            'text',
             Gio.SettingsBindFlags.DEFAULT);
-        this._settings.bind('image-width',
-            this._image_width,
-            'value',
+        this._settings.bind('query',
+            this._query,
+            'text',
             Gio.SettingsBindFlags.DEFAULT);
-        this._settings.bind('image-height',
-            this._image_height,
-            'value',
+        this._settings.bind('collections',
+            this._collections,
+            'text',
             Gio.SettingsBindFlags.DEFAULT);
-        this._settings.bind('keyword',
-            this._keyword,
+        this._settings.bind('topics',
+            this._topics,
             'text',
             Gio.SettingsBindFlags.DEFAULT);
 
-        this._unsplashUnconstrained(this._constraint_type, true, this._featured_only);
-        this._unsplashUnconstrained(this._constraint_type, false, this._constraint_value);
-        this._constraint_type.connect('notify::selected', (comboRow: Adw.ComboRow) => {
-            this._unsplashUnconstrained(comboRow, true, this._featured_only);
-            this._unsplashUnconstrained(comboRow, false, this._constraint_value);
+        // Setup combo rows with translatable UI elements
+        const setupComboRowTranslation = (combo: Adw.ComboRow, settingsId: string, mapping: Map<string, string>): void => {
+            const uiValues = Array.from(mapping.keys());
+            combo.model = Gtk.StringList.new(uiValues);
 
-            this._featured_only.set_active(false);
-        });
+            let selectedString = uiValues[combo.selected];
+            let settingsString = mapping.get(selectedString);
+            this._settings.setString(settingsId, settingsString ?? '');
+
+            combo.connect('notify::selected', () => {
+                selectedString = uiValues[combo.selected];
+                settingsString = mapping.get(selectedString);
+                this._settings.setString(settingsId, settingsString ?? '');
+            });
+        };
+        setupComboRowTranslation(this._content_filter, 'content-filter', this.contentFilterTranslationMap);
+        setupComboRowTranslation(this._orientation, 'orientation', this.orientationTranslationMap);
     }
 
     /**
-     * Switch element sensitivity based on a selected combo row entry.
+     * Explicit mapping of translated UI text to settings string.
      *
-     * @param {Adw.ComboRow} comboRow ComboRow with selected entry
-     * @param {boolean} enable Whether to make the element sensitive
-     * @param {Gtk.Widget} targetElement The element to target the sensitivity setting
+     * @returns {Map<string, string>} Map of id strings to translated strings.
      */
-    private _unsplashUnconstrained(comboRow: Adw.ComboRow, enable: boolean, targetElement: Gtk.Widget): void {
-        if (comboRow.selected === 0)
-            targetElement.set_sensitive(enable);
-        else
-            targetElement.set_sensitive(!enable);
-    }
+    contentFilterTranslationMap = ((): Map<string, string> => {
+        const mapping = new Map<string, string>();
+
+        Object.values(ContentFilter).forEach(filter => {
+            switch (filter) {
+            case ContentFilter.LOW:
+                mapping.set(_('Low'), ContentFilter.LOW);
+                break;
+            case ContentFilter.HIGH:
+                mapping.set(_('High'), ContentFilter.HIGH);
+                break;
+            default:
+                mapping.set('<unknown>', '<unknown>');
+                break;
+            }
+        });
+
+        return mapping;
+    })();
+
+    /**
+     * Explicit mapping to ensure xgettext recognizes the stings.
+     *
+     * @returns {Map<string, string>} Map of id strings to translated strings.
+     */
+    orientationTranslationMap = ((): Map<string, string> => {
+        const mapping = new Map<string, string>();
+
+        Object.values(Orientation).forEach(filter => {
+            switch (filter) {
+            case Orientation.PORTRAIT:
+                mapping.set(_('Portrait'), Orientation.PORTRAIT);
+                break;
+            case Orientation.LANDSCAPE:
+                mapping.set(_('Landscape'), Orientation.LANDSCAPE);
+                break;
+            case Orientation.SQUARISH:
+                mapping.set(_('Squarish'), Orientation.SQUARISH);
+                break;
+            default:
+                mapping.set('<unknown>', '<unknown>');
+                break;
+            }
+        });
+
+        return mapping;
+    })();
 
     /**
      * Clear all config options associated to this specific adapter.
@@ -126,52 +182,6 @@ class UnsplashSettings extends Adw.PreferencesPage {
     clearConfig(): void {
         this._settings.resetSchema();
     }
-}
-
-/**
- * Retrieve the human readable enum name.
- *
- * @param {ConstraintType} type The type to name
- * @returns {string} Name
- */
-function _getConstraintTypeName(type: ConstraintType): string {
-    let name: string;
-
-    switch (type) {
-    case ConstraintType.UNCONSTRAINED:
-        name = 'Unconstrained';
-        break;
-    case ConstraintType.USER:
-        name = 'User';
-        break;
-    case ConstraintType.USERS_LIKES:
-        name = 'User\'s Likes';
-        break;
-    case ConstraintType.COLLECTION_ID:
-        name = 'Collection ID';
-        break;
-
-    default:
-        name = 'Constraint type name not found';
-        break;
-    }
-
-    return name;
-}
-
-/**
- * Get a list of human readable enum entries.
- *
- * @returns {string[]} Array with key names
- */
-function getConstraintTypeNameList(): string[] {
-    const list: string[] = [];
-
-    const values = Object.values(ConstraintType).filter(v => !isNaN(Number(v)));
-    for (const i of values)
-        list.push(_getConstraintTypeName(i as ConstraintType));
-
-    return list;
 }
 
 export {UnsplashSettings};
